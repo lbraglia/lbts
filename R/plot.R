3d5fcba75a329f7096d284d1ee03ab71df23c7c5
Luca Braglia <lbraglia@gmail.com>
2026-05-02 14:20:09 +0200

    update

---
+++ R/plot.R
@@ -0,0 +1,16 @@
+# Plot of a single time series with standard diagnostic
+#'@export
+ts_plot <- function(x, lag.max=50, ...){
+  par(mfrow=c(1,3))
+  ts.plot(x, xlab="Time", ...)
+  acf(x,lag.max = lag.max, drop.lag.0 = FALSE, main="")
+  pacf(x,lag.max = lag.max, main="")
+}
+
+# Plotting of multiple time series
+#'@export
+
+mts_plot <- function(x, col=lbmisc::col2hex("black", 0.1), ...){
+  if (is.list(x)) x <- do.call(cbind, x)
+  ts.plot(x, xlab="Time", col=col, ...)
+}
---
+++ R/sim.R
@@ -0,0 +1,30 @@
+#'@export
+sim_ma1 <- function(n=100, omega=0, theta=0.5, sigma2=1){
+  e <- sqrt(sigma2) * rnorm(n) # epsilon_t sim N(0, sigma^2)
+  x <- omega # x[1]=omega: initialization of the process
+  for(t in seq(1, n-1)){
+    x[t+1] <- omega  + theta * e[t] + e[t+1]
+  }
+  x
+}
+
+#'@export
+sim_ar1 <- function (n = 100, omega = 0, psi = 0.5, sigma2 = 1) 
+{
+  e <- sqrt(sigma2) * rnorm(n) # white noise N(0, sigma2)
+  x <- omega/(1-psi) # x[1] initialisation matters when omega is nonzero
+  for (t in seq(1, n - 1)) {
+    x[t+1] <- omega + psi * x[t] + e[t+1]
+  }
+  x
+}
+
+
+#'@export
+sim_rw <- function (n = 100, x0 = 0, xt = rnorm) 
+{
+  x <- x0
+  xts <- xt(n=n) ## iid
+  rw <- cumsum(c(x0, xts))
+  rw[-length(rw)]
+}
