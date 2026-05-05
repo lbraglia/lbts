24138968ee66143fd574ac7d061a9d7acce40462
Luca Braglia <lbraglia@gmail.com>
2026-05-05 17:04:53 +0200

    update

--- R/sim.R
+++ R/sim.R
@@ -9,12 +9,12 @@ sim_ma1 <- function(n=100, omega=0, theta=0.5, sigma2=1){
 }
 
 #'@export
-sim_ar1 <- function (n = 100, omega = 0, psi = 0.5, sigma2 = 1) 
+sim_ar1 <- function (n = 100, omega = 0, phi = 0.5, sigma2 = 1) 
 {
   e <- sqrt(sigma2) * rnorm(n) # white noise N(0, sigma2)
-  x <- omega/(1-psi) # x[1] initialisation matters when omega is nonzero
+  x <- omega/(1-phi) # x[1] initialisation matters when omega is nonzero
   for (t in seq(1, n - 1)) {
-    x[t+1] <- omega + psi * x[t] + e[t+1]
+    x[t+1] <- omega + phi * x[t] + e[t+1]
   }
   x
 }
@@ -31,11 +31,11 @@ sim_rw <- function (n = 100, x0 = 0, xt = rnorm)
 
 
 #'@export
-sim_arma11 <- function(n=100, omega=0, theta=0.5, psi = 0.5, sigma2=1){
+sim_arma11 <- function(n=100, omega=0, theta=0.5, phi = 0.5, sigma2=1){
   e <- sqrt(sigma2) * rnorm(n) # epsilon_t sim N(0, sigma^2)
-  x <- omega/(1-psi) # initialization
+  x <- omega/(1-phi) # initialization
   for (t in seq(1, n-1)){
-    x[t+1] = omega + psi * x[t]   + theta * e[t]    + e[t+1]
+    x[t+1] = omega + phi * x[t]   + theta * e[t]    + e[t+1]
   }
   x
 }
